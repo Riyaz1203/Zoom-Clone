a978c114df616632abff1f2e95a324adc720cca1
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2024-01-24 15:58:36 +0530

    Space added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/history_meeting_screen.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import 'meeting_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChange(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  List<Widget> pages = [
+    const MeetingScreen(),
+    const HistoryMeetingScreen(),
+    const Text('Contacts'),
+    const Text('Settings')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14,
+        onTap: onPageChange,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.comment_bank), label: 'Meet & Chat'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.lock_clock), label:      'Meetings'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: 'Contacts'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined), label: 'Settings'),
+        ],
+      ),
+    );
   }
 }
